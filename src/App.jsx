d3d4a0bf48530aa521c625c2be784270fcf0d9f2
winsonken <winson.kennedy222@gmail.com>
2024-03-03 12:53:58 +0700

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from './pages/Login';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
