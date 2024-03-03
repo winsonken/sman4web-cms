85d1bc8a5b4e243c62a70c32f05fd08670475738
winsonken <winson.kennedy222@gmail.com>
2024-03-03 16:19:42 +0700

    slicing login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,86 @@
-import React from 'react';
+import React, { useState } from 'react';
+import logo from '../assets/logo-sman4.png';
+import { FaEye } from 'react-icons/fa';
+import { FaEyeSlash } from 'react-icons/fa';
 
 const Login = () => {
-  return <div>Login</div>;
+  const [showPassword, setShowPassword] = useState(true);
+
+  const handlePassword = () => {
+    setShowPassword(!showPassword);
+  };
+
+  return (
+    <div className="w-screen h-screen flex justify-center items-center p-3">
+      <div className="w-full max-w-sm h-fit flex flex-col gap-5 bg-main-blue px-5 py-8 rounded-xl">
+        <div className="flex flex-col items-center gap-3">
+          <div className="w-40">
+            <img src={logo} alt="SMAN4 Logo" />
+          </div>
+
+          <h1 className="text-xl font-semibold text-second-blue xs:text-2xl">
+            Sistem Database SMAN4
+          </h1>
+        </div>
+
+        <div>
+          <form>
+            <div className="flex flex-col gap-3">
+              <label
+                htmlFor="username"
+                className="text-base font-medium text-second-blue"
+              >
+                Username
+              </label>
+              <input
+                type="text"
+                id="username"
+                placeholder="Username"
+                className="bg-transparent py-2 border-b border-main-cream placeholder-main-cream placeholder:font-medium focus:outline-0 caret-second-blue"
+                autoComplete="off"
+                style={{ color: '#FFFDDE' }}
+              />
+            </div>
+
+            <div className="flex flex-col gap-3 mt-5">
+              <label
+                htmlFor="password"
+                className="text-base font-medium text-second-blue"
+              >
+                Password
+              </label>
+
+              <div className="relative">
+                <input
+                  type={showPassword ? 'password' : 'text'}
+                  id="password"
+                  placeholder="Password"
+                  className="w-full bg-transparent py-2 border-b border-main-cream placeholder-main-cream placeholder:font-medium focus:outline-0 caret-second-blue"
+                  style={{ color: '#FFFDDE' }}
+                />
+
+                <div
+                  className="text-xl text-main-cream absolute top-1/2 right-0 -translate-y-1/2 cursor-pointer"
+                  onClick={handlePassword}
+                >
+                  {showPassword ? <FaEye /> : <FaEyeSlash />}
+                </div>
+              </div>
+            </div>
+          </form>
+        </div>
+
+        <div>
+          <button
+            type="submit"
+            className="w-full text-base font-medium text-white bg-second-blue py-3 rounded"
+          >
+            Login
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        xs: '480px',
+      },
+      colors: {
+        white: '#FFFFFF',
+        'main-blue': '#7FC7D9',
+        'second-blue': '#146C94',
+        'main-cream': '#FFFDDE',
+      },
+    },
   },
   plugins: [],
 };
