4a0c93871762ab55fff11f5d202fc3783a110be0
= <=>
2024-03-20 18:47:59 +0700

    merge branch jonathan and winson

--- src/components/angkatan/FormEditAngkatan.jsx
+++ src/components/angkatan/FormEditAngkatan.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Input from '../Input';
 import SelectInput from '../SelectInput';
 import Button from '../Button';
@@ -6,55 +6,136 @@ import { useForm, Controller } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as yup from 'yup';
 import { toast } from 'react-toastify';
+import { useUpdateAngkatanMutation } from '../../services/api/angkatanApiSlice';
 
 const validationSchema = yup
   .object({
     no_angkatan: yup.string().required('No Angkatan is required'),
     tahun: yup.string().required('Tahun is required'),
-    status_angkatan: yup.object().required('Status is required'),
+    // status_angkatan: yup.object().required('Status is required'),
   })
   .required();
 
 const FormEditAngkatan = (props) => {
-  const { setIsOpenPopUpEdit } = props;
+  const { data, setIsOpenPopUpEdit } = props;
+
   const {
     control,
     register,
     handleSubmit,
     formState: { errors },
+    clearErrors,
+    setValue,
   } = useForm({
     resolver: yupResolver(validationSchema),
   });
 
-  const onSubmit = (e) => {
-    toast.success('Angkatan berhasil diubah!', {
-      position: 'top-right',
-      theme: 'light',
-    });
+  const initialFormInput = {
+    id_angkatan: '',
+    no_angkatan: '',
+    tahun: '',
+    status_angkatan: '',
   };
 
+  const [formInput, setFormInput] = useState(initialFormInput);
+  const [selectedStatusValue, setSelectedStatusValue] = useState('');
+  const [updateAngkatan, { isLoading, isError, error }] =
+    useUpdateAngkatanMutation();
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormInput((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmitForm = async () => {
+    const payload = {
+      id_angkatan: formInput?.id_angkatan,
+      no_angkatan: formInput?.no_angkatan,
+      tahun: formInput?.tahun,
+      status_angkatan: selectedStatusValue,
+    };
+
+    try {
+      const response = await updateAngkatan(payload).unwrap();
+
+      if (!response.error) {
+        toast.success('Angkatan berhasil diubah!', {
+          position: 'top-right',
+          theme: 'light',
+        });
+        setIsOpenPopUpEdit(false);
+      }
+    } catch (error) {
+      const errorMessage = error?.data?.message;
+      toast.error(`${errorMessage}`, {
+        position: 'top-right',
+        theme: 'light',
+      });
+    }
+  };
+
+  useEffect(() => {
+    if (data) {
+      setFormInput({
+        id_angkatan: data?.id_angkatan,
+        no_angkatan: data?.no_angkatan,
+        tahun: data?.tahun,
+        status_angkatan: data?.status_angkatan,
+      });
+    }
+    setSelectedStatusValue(data?.status_angkatan);
+  }, [data, setFormInput]);
+
+  useEffect(() => {
+    const fieldsToCheck = ['no_angkatan', 'tahun', 'status_angkatan'];
+
+    fieldsToCheck.forEach((field) => {
+      if (errors[field] && formInput[field] !== '') {
+        if (errors[field].type === 'required') {
+          clearErrors(field);
+        }
+      }
+    });
+  }, [formInput, clearErrors, errors]);
+
+  useEffect(() => {
+    if (data) {
+      setValue('no_angkatan', data?.no_angkatan);
+      setValue('tahun', data?.tahun);
+      setValue('status_angkatan', data?.status_angkatann);
+    }
+  }, [data, setFormInput, setValue]);
+
   const statusAngkatan = [
-    { value: '0', label: 'Belum dimulai' },
-    { value: '1', label: 'Aktif' },
-    { value: '2', label: 'Lulus' },
+    { value: 0, label: 'Belum dimulai' },
+    { value: 1, label: 'Aktif' },
+    { value: 2, label: 'Lulus' },
   ];
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
+    <form onSubmit={handleSubmit(handleSubmitForm)}>
       <div className="flex flex-col gap-6">
         <div className="flex flex-col gap-3">
           <Input
             type="number"
             label="Angkatan ke-"
             name="no_angkatan"
+            value={formInput?.no_angkatan}
+            onChange={handleChange}
             register={register}
             errors={errors}
           />
 
           <Input
+            type="number"
             label="Tahun"
             name="tahun"
             register={register}
+            value={formInput?.tahun}
+            onChange={handleChange}
             errors={errors}
           />
 
@@ -67,6 +148,8 @@ const FormEditAngkatan = (props) => {
                 data={statusAngkatan}
                 label="Status"
                 name="status_angkatan"
+                selectedValue={selectedStatusValue}
+                setSelectedValue={setSelectedStatusValue}
                 placeholder="Select status"
                 errors={errors}
               />
